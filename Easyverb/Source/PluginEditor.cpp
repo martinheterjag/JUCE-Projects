5842a4591eafd550534d1a3e87f39e41f9d456b1
martinheterjag <martine7@kth.se>
2022-03-07 21:26:36 +0100

    Change knob color and header font

--- Easyverb/Source/PluginEditor.cpp
+++ Easyverb/Source/PluginEditor.cpp
@@ -22,6 +22,7 @@ EasyverbAudioProcessorEditor::EasyverbAudioProcessorEditor (EasyverbAudioProcess
         addAndMakeVisible(*component);
     }
 
+    reverb_slider_.setColour(juce::Slider::thumbColourId, juce::Colours::chocolate);
     reverb_slider_.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
     reverb_slider_.setTextBoxStyle(juce::Slider::NoTextBox, true, TEXT_BOX_SIZE, TEXT_BOX_SIZE);
     reverb_slider_.addListener(this);
@@ -30,6 +31,7 @@ EasyverbAudioProcessorEditor::EasyverbAudioProcessorEditor (EasyverbAudioProcess
     reverb_slider_attachment_ = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>
         (audioProcessor.apvts, "REVERB", reverb_slider_);
 
+    mix_slider_.setColour(juce::Slider::thumbColourId, juce::Colours::chocolate);
     mix_slider_.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
     mix_slider_.setTextBoxStyle(juce::Slider::NoTextBox, true, TEXT_BOX_SIZE, TEXT_BOX_SIZE);
     mix_slider_.addListener(this);
@@ -55,13 +57,13 @@ void EasyverbAudioProcessorEditor::paint (juce::Graphics& g)
     g.fillRect(top_section_);
 
     
-    g.setColour (juce::Colour(0xff123456));
-    g.setFont(40.0f);
+    g.setColour (juce::Colour(juce::Colours::chocolate));
+    g.setFont(juce::Font(40.0f, juce::Font::bold));
     g.drawFittedText("EASYVERB", getLocalBounds(), juce::Justification::centredTop, 1);
 
     g.setColour(juce::Colours::darkorange);
     SetupSections();
-    g.setFont(18.0f);
+    g.setFont(juce::Font(18.0f));
     g.drawFittedText("REVERB", reverb_text_section_, juce::Justification::centred, 1);
     g.drawFittedText("DRY/WET", mix_text_section_, juce::Justification::centred, 1);
 }
